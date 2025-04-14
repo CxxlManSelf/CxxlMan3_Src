834f1ec8736e40f77c8fc13832b1d08b01bdd985
CxxlMan3 <cxxlman3@hotmail.com>
2025-04-14 22:49:59 +0800

    修改 20250411 使其符合新的核心規格

--- test/cpp/20250411-測試 LifeOwner 循環參照的處理能力/main.cpp
+++ test/cpp/20250411-測試 LifeOwner 循環參照的處理能力/main.cpp
@@ -15,8 +15,7 @@ class MyClassA : public LifeRes<LifeResType::ALL>
 public:    
     // Constructor
     MyClassA() 
-        : m_myB(std::shared_ptr<MyClassB>(nullptr),
-            this,
+        : m_myB(this,
             [this](CxxlMan3::LifeOwner<MyClassB> *myB, void *pChk)
             {
                 std::lock_guard<std::mutex> lock(m_mutex);
@@ -28,7 +27,7 @@ public:
     // Destructor
     ~MyClassA()
     {
-        std::cout << "MyClassA destructor" << std::endl;
+        std::cout << "MyClassA destructor\n";
     }
 
     void cxxlFASTCALL addMyB(const std::shared_ptr<MyClassB> &myB_ptr)
@@ -46,8 +45,7 @@ public:
 public:
     // Constructor
     MyClassB() 
-        : m_myA(std::shared_ptr<MyClassA>(),
-            this,
+        : m_myA(this,
             [this](CxxlMan3::LifeOwner<MyClassA> *myA,void *pChk)
             {
                 std::lock_guard<std::mutex> lock(m_mutex);
@@ -59,7 +57,7 @@ public:
     // Destructor
     ~MyClassB()
     {
-        std::cout << "MyClassB destructor" << std::endl;
+        std::cout << "MyClassB destructor\n";
     }
     
     void cxxlFASTCALL addMyA(const std::shared_ptr<MyClassA> &myA_ptr)
@@ -75,8 +73,7 @@ class MyRoot : public LifeRes<LifeResType::ONE>
 public:
     // Constructor
     MyRoot() 
-        : m_myA( std::shared_ptr<MyClassA>(),
-            this,
+        : m_myA(this,
             [this](CxxlMan3::LifeOwner<MyClassA> *myA,void *pChk)
             {
                 std::lock_guard<std::mutex> lock(m_mutex);
@@ -88,7 +85,7 @@ public:
     // Destructor
     ~MyRoot()
     {
-        std::cout << "MyRoot destructor" << std::endl;
+        std::cout << "MyRoot destructor\n";
     }
 
     void cxxlFASTCALL addMyA(const std::shared_ptr<MyClassA> &myA_ptr)
@@ -101,36 +98,36 @@ int main(int, char**)
 {
     {
         std::shared_ptr<ICore> core_ptr = getDestructor();
-        std::cout << "已經取得核心銷毁控制器" << std::endl;
-        std::cout << "按 <enter> 鍵繼續" << std::endl; std::cin.get();
+        std::cout << "已經取得核心銷毁控制器\n";
+        std::cout << "按 <enter> 鍵繼續\n"; std::cin.get();
     
         std::shared_ptr<MyClassA> myA_ptr = std::make_shared<MyClassA>();
         std::shared_ptr<MyClassB> myB_ptr = std::make_shared<MyClassB>();
-        std::cout << "已經建立了 myA_ptr 和 myB_ptr" << std::endl;
-        std::cout << "按 <enter> 鍵繼續" << std::endl; std::cin.get();
+        std::cout << "已經建立了 myA_ptr 和 myB_ptr\n";
+        std::cout << "按 <enter> 鍵繼續\n"; std::cin.get();
     
         myA_ptr->addMyB(myB_ptr);
         myB_ptr->addMyA(myA_ptr);
-        std::cout << "已經讓 myA_ptr 和 myB_ptr 互相引用" << std::endl;
-        std::cout << "按 <enter> 鍵繼續" << std::endl; std::cin.get();
+        std::cout << "已經讓 myA_ptr 和 myB_ptr 互相引用\n";
+        std::cout << "按 <enter> 鍵繼續\n"; std::cin.get();
     
         std::shared_ptr<MyRoot> myRoot_ptr = std::make_shared<MyRoot>();
-        std::cout << "已經建立了 myRoot_ptr" << std::endl;
-        std::cout << "按 <enter> 鍵繼續" << std::endl; std::cin.get();
+        std::cout << "已經建立了 myRoot_ptr\n";
+        std::cout << "按 <enter> 鍵繼續\n"; std::cin.get();
     
         myRoot_ptr->addMyA(myA_ptr);
-        std::cout << "已經讓 myRoot_ptr 持有 myA_ptr" << std::endl;
-        std::cout << "按 <enter> 鍵繼續" << std::endl; std::cin.get();
+        std::cout << "已經讓 myRoot_ptr 持有 myA_ptr\n";
+        std::cout << "按 <enter> 鍵繼續\n"; std::cin.get();
         
         myA_ptr.reset();
         myB_ptr.reset();
-        std::cout << "已經釋放了 myA_ptr 和 myB_ptr" << std::endl;
-        std::cout << "但彼此仍互相引用" << std::endl;
+        std::cout << "已經釋放了 myA_ptr 和 myB_ptr\n";
+        std::cout << "但彼此仍互相引用\n";
     
-        std::cout << "按 <enter> 鍵後將會釋放 myRoot_ptr" << std::endl; std::cin.get();    
+        std::cout << "按 <enter> 鍵後將會釋放 myRoot_ptr\n"; std::cin.get();    
         myRoot_ptr.reset();    
     }
     
-    std::cout << "已經演釋完整個過程" << std::endl;
-    std::cout << "按 <enter> 鍵結束程式" << std::endl; std::cin.get();
+    std::cout << "已經演釋完整個過程\n";
+    std::cout << "按 <enter> 鍵結束程式\n"; std::cin.get();
 }
