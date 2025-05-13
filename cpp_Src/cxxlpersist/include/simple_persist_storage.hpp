54b6fdd9a12f8eba385761a6045c500f922da528
CxxlMan3 <cxxlman3@hotmail.com>
2025-05-13 17:52:37 +0800

    暫存

--- cpp_Src/cxxlpersist/include/simple_persist_storage.hpp
+++ cpp_Src/cxxlpersist/include/simple_persist_storage.hpp
@@ -6,7 +6,7 @@
  *
  * Author: CxxlMan
  * Date: 2025 -
-******************************************************/
+ ******************************************************/
 #ifndef __CXXLPERSIST_SIMPLE_PERSIST_STORAGE_HPP_CxxlMan3
 #define __CXXLPERSIST_SIMPLE_PERSIST_STORAGE_HPP_CxxlMan3
 
@@ -14,13 +14,14 @@
 #include <stdfloat>
 #include <memory>
 
-#include <cxxlpersist.hpp>
-#include <persist_storage.hpp>
+#include "cxxlpersist.hpp"
+#include "persist_storage.hpp"
+#include "treenode.hpp"
 
 namespace CXXL
 {
 
-    // ISimplePersistStorage 的容器介面
+    // ISimplePersistStorage 使用的容器介面
     class ISimplePersistContainer
     {
 
@@ -60,7 +61,6 @@ namespace CXXL
 
         virtual void cxxlFASTCALL
         save(std::float128_t *p, size_t count) = 0;
-        
 
         // 讀取
         virtual bool cxxlFASTCALL
@@ -95,32 +95,48 @@ namespace CXXL
 
         virtual bool cxxlFASTCALL
         load(std::float128_t *p, size_t count) = 0;
-
     };
 
-    std::shared_ptr<ISimplePersistContainer> 
-    CXXLPERSIST_DLLEXPORT defaultSimplePersistContainer();
-
-
-    class ISimplePersistStorage: public IPersistStorage
+    // 提供一個包裹 TreeNode<std::string> 的 ISimplePersistContainer 容器實作
+    class ISimplePersistContainer_StringTreeNode : public ISimplePersistContainer
     {
-    protected:        
+    protected:
+        ISimplePersistContainer_StringTreeNode() = default;
+
+    public:
+        virtual ~ISimplePersistContainer_StringTreeNode() {}
+
+        // 取得 TreeNode<std::string>
+        virtual std::shared_ptr<TreeNode<std::string>> cxxlFASTCALL getTreeNode() const = 0;
+
+        static std::shared_ptr<ISimplePersistContainer>
+            CXXLPERSIST_DLLEXPORT create(const std::shared_ptr<TreeNode<std::string>> &treeNode_ptr =
+                                             std::make_shared<TreeNode<std::string>>());
+    };
+
+
+    // 提供一個簡易版的 IPersistStorage 實作
+    class ISimplePersistStorage : public IPersistStorage
+    {
+    protected:
         ISimplePersistStorage() = default;
-    public:        
+
+    public:
         virtual ~ISimplePersistStorage() {}
 
         virtual bool cxxlFASTCALL save(IPersistChannel *pPersistable) override;
         virtual bool cxxlFASTCALL load(IPersistChannel *pPersistable) override;
 
+        virtual std::shared_ptr<ISimplePersistContainer>
+            cxxlFASTCALL getSimplePersistContainer() const = 0;
 
-        static std::shared_ptr<IPersistStorage> 
-        CXXLPERSIST_DLLEXPORT create();
+        static std::shared_ptr<IPersistStorage>
+            CXXLPERSIST_DLLEXPORT create(const std::shared_ptr<ISimplePersistContainer> &container_ptr = 
+                ISimplePersistContainer_StringTreeNode::create());
     };
 
-
-    class SimpleSerialize: public ISerialize
+    class SimpleSerialize : public ISerialize
     {
-
     };
 
 } // namespace CXXL
